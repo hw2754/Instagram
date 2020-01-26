606420636d1520ee908780cac162539137f46d3d
hw2754 <873086019@qq.com>
2020-01-26 16:27:35 -0500

    post

--- static_pages/models.py
+++ static_pages/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from imagekit.models import ProcessedImageField
 # Create your models here.
+# define a kind of data "Post", it has two attributes
+class Post(models.Model):
+    title = models.TextField(blank=True , null=True)
+    image = ProcessedImageField(
+        #when user upload a image, it will be stored in this path
+        upload_to = 'static/images/posts',
+        format = 'JPEG',
+        options = {'quality': 100},
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.title
